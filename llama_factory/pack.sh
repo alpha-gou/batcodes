59fb122fb26f94e1e38ecdd049a836e7b0c28163
alpha-gou <wangning40@zuoyebang.com>
2025-09-28 15:59:22 +0800

    update

--- llama_factory/pack.sh
+++ llama_factory/pack.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # run:
-# > sh pach.sh project_name
+# > sh pach.sh [project_name]
 
-cp -r code_files $1
+cp -r project_template $1
 cp ../vllm_server/run_vllm_server.sh $1/
 zip -r $1.zip $1
 rm -rf $1
